5154caa314c440d6589161e3beb7e80fee7671bb
jacson2045 <yaxonpro_2045@hotmail.com>
2024-05-23 01:35:41 -0500

    agregando 23 de mayo

--- SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/Categoria.java
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/Categoria.java
@@ -24,7 +24,7 @@ public class Categoria {
     @Column(name = "id_categoria")
     private Integer idCategoria;
     @Basic(optional = false)
-    @Column(name = "nombre")
+    @Column(name = "nombres")
     private String nombre;
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria")
